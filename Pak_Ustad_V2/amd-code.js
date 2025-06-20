d573500ec90fad35f18764902f3daa58ae2af53a
Lemon <grouplemon0@gmail.com>
2025-06-20 10:30:12 +0700

    Create amd-code.js

---
+++ Pak_Ustad_V2/amd-code.js
@@ -0,0 +1,30 @@
+define(['axios'], function (axios) {
+    const handler = async (m, { conn, text, usedPrefix, command }) => {
+        if (!text) return m.reply(`Example:\n${usedPrefix + command} Makan Sambil Kuyang Bisa Gak Pak Ustad`);
+        if (text.length > 68) return m.reply(`Teks tidak boleh lebih dari 68 karakter`);
+
+        try {
+            const response = await axios.post('https://lemon-ustad.vercel.app/api/generate-image', {
+                isi: text,
+                option: "type2" // hanya tersedia type1 dan type2
+            }, {
+                responseType: 'arraybuffer',
+                headers: { 'Content-Type': 'application/json' }
+            });
+
+            await conn.sendMessage(m.chat, {
+                image: Buffer.from(response.data)
+            }, { quoted: m });
+
+        } catch (err) {
+            console.error(err);
+            m.reply('Terjadi kesalahan saat menghubungi server.');
+        }
+    };
+
+    handler.command = ['pak-ustad1'];
+    handler.tags = ['fun'];
+    handler.help = ['pak-ustad1'];
+
+    return handler;
+});
