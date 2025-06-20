a147c5f478f610e205f07eb7c1f5ee4bf74209fd
Lemon <grouplemon0@gmail.com>
2025-06-20 10:22:32 +0700

    Create _main-kode.js

---
+++ Pak_Ustad_V2/_main-kode.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const axios = require('axios')
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  const { text } = req.query
+  if (!text) return res.status(400).json({ status: false, message: 'Masukkan query text' })
+
+  try {
+    const response = await axios.post('https://lemon-welcome.vercel.app/api/generate-image', {
+      isi,
+      option: "type1" // hanya tersedia type1 dan type2
+    }, {
+      responseType: 'arraybuffer',
+      headers: { 'Content-Type': 'application/json' }
+    })
+
+    res.set('Content-Type', 'image/png')
+    res.send(response.data)
+  } catch (error) {
+    res.status(500).json({ status: false, message: 'Gagal generate gambar' })
+  }
+})
+
+module.exports = router
